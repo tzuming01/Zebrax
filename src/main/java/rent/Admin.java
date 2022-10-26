b6da98abd1621d3c5ffc53c7fa6245a47a7613bc
AuniShah <radennn.auni@gmail.com>
2022-10-26 19:36:50 +0800

    Sign in functionalities
    Added text file for user admin, remove all the images on the UI (so only left, the labels for later).

---
+++ src/main/java/rent/Admin.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author aunishah
+ */
+public class Admin {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        
+       
+    }
+    
+}
